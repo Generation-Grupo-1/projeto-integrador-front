15245972d7904d17060311f5fa3158865f467ff0
Jonas <jonasdanova@gmail.com>
2023-11-28 10:44:44 -0300

    Navbar completamente resposiva

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,14 +8,14 @@ function Header() {
             justify-center py-3 bg-cyan-500 text-white w-400 text-sm max-w-6x1 mx-auto px-4 sm:px-5 lg:px-7"
       >
         <div className="flex justify-center">
-          <form className="px-4 rounded-md bg-white flex items-center">
+          <form className="px-4 rounded-md w-80 bg-white flex items-center">
 
           <button
               className="text-black bg-transparent border-none cursor-pointer flex items-center justify-center"
               type="submit"
               aria-label="Iniciar pesquisa"
             >
-              <MagnifyingGlass size={16} />
+              <MagnifyingGlass size={18} />
             </button>
             <input
               className="bg-white border-none rounded-md p-2 w-48 text-black outline-none"
